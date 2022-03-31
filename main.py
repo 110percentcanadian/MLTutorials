f916498d8a4961f319d973430ec6bba400121296
jeff <jeffdoyle17@gmail.com>
2022-03-30 21:50:40 -0700

    progress in ML tutorial

--- main.py
+++ main.py
@@ -1,16 +1,136 @@
-# This is a sample Python script.
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor, Lambda
+import os
+import pandas as pd
+from torchvision.io import read_image
+from torch.utils.data import DataLoader, Dataset
+import matplotlib.pyplot as plt
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# creating custom datasets
+class CustomImageDataset(Dataset):
+    def __init__(self, annotations_file,img_dir, transform = None, target_transform = None ):
+        self.img_labels = pd.read_csv(annotations_file)
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, item, idx):
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx,0])
+        image = read_image(img_path)
+        label = self.img_labels.iloc[idx,1]
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image, label
+
+# get some data
+training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor(),
+)
+
+# Download test data from open datasets.
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor(),
+)
+
+batch_size = 64
+
+# Create data loaders.
+train_dataloader = DataLoader(training_data, batch_size=batch_size, shuffle = True)
+test_dataloader = DataLoader(test_data, batch_size=batch_size, shuffle = True)
+
+train_features, train_labels =next(iter(train_dataloader))
+print(f"Feature batch shape: {train_features.size()}")
+print(f"labels batch shape: {train_labels.size()}")
+img = train_features[0].squeeze()
+label = train_labels[0]
+plt.imshow(img, cmap="gray")
+plt.show()
+print(f"Label: {label}")
+
+# transform to get data into a tensors
+
+ds = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor(),
+    target_transform=Lambda(lambda y:torch.zeros(10,dtype=torch.float).scatter_(0,torch.tensor(y), value=1))
+)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+for X, y in test_dataloader:
+    print(f"Shape of X [N, C, H, W]: {X.shape}")
+    print(f"Shape of y: {y.shape} {y.dtype}")
+    break
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Get cpu or gpu device for training.
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using {device} device")
+
+# Define model
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10)
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+# model = NeuralNetwork().to(device)
+# print(model)
+
+#tutorial section on visualizing data set
+
+labels_map = {
+    0: "T-Shirt",
+    1: "Trouser",
+    2: "Pullover",
+    3: "Dress",
+    4: "Coat",
+    5: "Sandal",
+    6: "Shirt",
+    7: "Sneaker",
+    8: "Bag",
+    9: "Ankle Boot",
+}
+
+
+# figure = plt.figure(figsize=(8,8))
+# cols, rows = 3,3
+# for i in range(1, cols*rows+1):
+#     # grab a random sample
+#     sample_idx = torch.randint(len(training_data), size=(1,)).item()
+#     # grab image and label from sample
+#     img, label = training_data[sample_idx]
+#     figure.add_subplot(rows, cols, i)
+#     #names plot label from training set
+#     plt.title(labels_map[label])
+#     plt.axis("off")
+#     plt.imshow(img.squeeze(), cmap="gray")
+#
+# plt.show()
